cef6d93668c7381d1f202aaa697838aea7e890bb
keddib <keddib@student.1337.ma>
2021-07-03 18:13:21 +0100

    intro to I/O universal : program that copy file1 to file2

--- main.c
+++ main.c
@@ -1,23 +1,41 @@
 #include <unistd.h>
-     #include <sys/wait.h>
-		#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+     #include <sys/types.h>
+     #include <sys/uio.h>
+
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
-		return 0;
-	
-	pid_t pid;
-	int ex = 10;
-	
-	pid = fork();
-	
-	if (pid > 0)
+	int input;
+	int output;
+	int ret;
+	char buf[1024];
+
+	if (argc != 3 || strcmp(argv[1], "--help") == 0)
 	{
-		execv(argv[1], argv + 1);
-	}
-	else
+		write(2, "copy [old-file] [new-file]\n", 27); 
 		return (1);
-	waitpid(pid, &ex, 0);
-	return (10);
+	}
+	input = open(argv[1], O_RDONLY);
+	if (input == -1)
+	{
+		write(2, "open file input\n", 16);
+		return (1);
+	}
+	output = open (argv[2], O_CREAT | O_WRONLY | O_APPEND, 0666); 
+	if (output == -1)
+	{
+		write(1, "opening file output\n", 19);
+		return (1);
+	}
+	while ((ret = read(input, buf, 1024)) > 0)
+		write(output, buf, ret);
+	if (ret == -1)
+		write(2, "READ ERR\n", 9);
+	if (close(input) == -1)
+		write(2, "CLOSE IN\n", 9);
+	if (close(output) == -1)
+		write(2, "CLOSE OUT\n", 10);
+	return (0);
 }
