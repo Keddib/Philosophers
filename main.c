d4fe50a3b2b88c1b5db94a9c4e9ccbc30bb5f25a
keddib <keddib@student.1337.ma>
2021-07-07 15:06:33 +0100

    hello world process creation

--- main.c
+++ main.c
@@ -1,41 +1,33 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-     #include <sys/types.h>
-     #include <sys/uio.h>
-
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	int input;
-	int output;
-	int ret;
-	char buf[1024];
+	pid_t	pid;
+	int		status;
 
-	if (argc != 3 || strcmp(argv[1], "--help") == 0)
+
+	if (argc < 2)
+		return (0);
+	status = 0;
+	argv++;
+	pid = fork();
+	if (pid == 0)
 	{
-		write(2, "copy [old-file] [new-file]\n", 27); 
-		return (1);
+		puts("Child process running ...\n");
+		execve(argv[0], argv, NULL);
+		exit(20);
 	}
-	input = open(argv[1], O_RDONLY);
-	if (input == -1)
+	else
 	{
-		write(2, "open file input\n", 16);
-		return (1);
+		wait(&status);
+		puts("Parent process running ...\n");
+		printf("status = %d\n", status);
 	}
-	output = open (argv[2], O_CREAT | O_WRONLY | O_APPEND, 0666); 
-	if (output == -1)
-	{
-		write(1, "opening file output\n", 19);
-		return (1);
-	}
-	while ((ret = read(input, buf, 1024)) > 0)
-		write(output, buf, ret);
-	if (ret == -1)
-		write(2, "READ ERR\n", 9);
-	if (close(input) == -1)
-		write(2, "CLOSE IN\n", 9);
-	if (close(output) == -1)
-		write(2, "CLOSE OUT\n", 10);
 	return (0);
 }
